1995be65013ed68a61f4575f5cbef43115b18fc3
HyunJung <HyunJungC.Dev@gmail.com>
2021-03-25 15:45:28 +0900

    feat: add fizz
    if number is multiples of 3, print fizz

--- fb.py
+++ fb.py
@@ -4,6 +4,10 @@ def do_fizzbuzz():
     while curr <= n:
         if curr % 3 == 0 and curr % 5 == 0:
             print(curr," : fizzbuzz")
+        elif curr % 3 == 0:
+            print(curr, " : fizz")
+
         curr += 1
+        
 
 do_fizzbuzz()
